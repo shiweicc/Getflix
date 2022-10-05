5499065debeb2319083cff75291dbc409e4881fb
adilovm <miras9191@gmail.com>
2022-10-05 15:16:51 -0700

    initial commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 3001;
+const path = require('path');
+app.use(express.json())
+app.use(express.urlencoded({extended: false}));
+
+app.get('/test', (req, res)=>{
+  res.json({message:'Welcome to Getflix!'})
+})
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`)
+})
