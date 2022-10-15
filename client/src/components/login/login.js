80d48e8a2bfbb72892061d528fae0d1af1fd70b8
Chris <chrisbaharians@gmail.com>
2022-10-15 15:10:50 -0600

    Added interaction with DB

--- client/src/components/login/login.js
+++ client/src/components/login/login.js
@@ -2,12 +2,17 @@ import React from 'react';
 import { useState } from 'react';
 import ReactDOM from 'react-dom';
 import logo from '/Users/chrisbaharians/RPP36/getflix/client/src/getflixLogo.png'
+import Main from '../main/main.js'
+import { Navigate } from 'react-router-dom';
+import axios from 'axios';
 
 import './login.css'
 
 const Login = () => {
   const [errorMessages, setErrorMessages] = useState({})
   const [isSubmitted, setIsSubmitted] = useState(false);
+  const [isClicked, setClicked] = useState(false)
+  const LOGIN_URL = 'http://localhost:3001/login'
 
   const renderErrorMessage = (name) => {
     name === errorMessages.name && (
@@ -31,27 +36,51 @@ const Login = () => {
     pass: 'invalid password'
   }
 
+  // const handleClick = (event) => {
+  //   event.preventDefault();
+
+  //   if(isClicked)
+  // }
 
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
 
     var { uname, pass } = document.forms[0];
+    var username = uname.value;
+    var password = pass.value;
 
-    const userData = database.find((user) => user.username === uname.value)
-    console.log('this is the userData', userData)
-
-    if (userData) {
-      if (userData.password !== pass.value) {
-        setErrorMessages({name: 'pass', message: errors.pass});
-
-      } else {
+    try {
+      const response = await axios.post(LOGIN_URL,
+          JSON.stringify({ username, password }),
+          {
+              headers: { 'Content-Type': 'application/json' },
+              withCredentials: false
+          }
+      );
+      if(response.status === 200) {
         setIsSubmitted(true);
+      } else {
+        console.log('incorrect credentials');
       }
-    } else {
-      setErrorMessages({name: "uname", message: errors.uname});
-      alert('Username does not exist')
-    }
+  }
+  catch(err) {
+    console.log(err);
+  }
+
+    // const userData = database.find((user) => user.username === uname.value)
+
+    // if (userData) {
+    //   if (userData.password !== pass.value) {
+    //     setErrorMessages({name: 'pass', message: errors.pass});
+    //     alert('Password does not match')
+    //   } else {
+        // setIsSubmitted(true);
+    //   }
+    // } else {
+    //   setErrorMessages({name: "uname", message: errors.uname});
+    //   alert('Username does not exist')
+    // }
   };
 
   const renderForm = (
@@ -73,17 +102,21 @@ const Login = () => {
   )
 
   return (
-    <div className='app'>
-      <div className='top'>
-        <img src={logo} alt='getflix-logo' className='logo' />
-        <button type='button' className='back-btn'>Back</button>
-      </div>
-      <div className='login-form'>
-        <div className='title'>LOG IN</div>
-        {isSubmitted ? <div>User is successfully logged in!</div> : renderForm}
-      </div>
+    <>
+    {isSubmitted ? <Navigate to='/main' /> :  <div className='app'>
+    <div className='top'>
+      <img src={logo} alt='getflix-logo' className='logo' />
+      {isClicked ? <Navigate to='main' /> : <button type='button' className='back-btn'>Back</button>}
     </div>
-  )
+    <div className='login-form'>
+      <div className='title'>LOG IN</div>
+      {isSubmitted ? <div>User is successfully logged in!</div> : renderForm}
+    </div>
+  </div>
+    }
+    </>
+)
+
 }
 
 export default Login;
--- server/server.js
+++ server/server.js
@@ -7,12 +7,17 @@ const path = require('path');
 app.use(express.json())
 app.use(express.urlencoded({extended: false}));
 const fakeData = require('../client/src/fakeData/fakeMovies.js')
+const cors = require('cors');
+const { pool } = require('./authConfig.js');
+const bcrypt = require('bcrypt');
+
+app.use(cors());
 
 app.get('/test', (req, res)=>{
   res.json({message:'Welcome to Getflix!'})
 })
 
-app.get('/main', (req,res) => {
+app.get('/main', checkNotAuthenticated, (req,res) => {
   //set up to go to microservice later
   res.send(fakeData.movies)
 })
@@ -21,6 +26,50 @@ app.get('/profile', (req,res) => {
   //set up to go to microservice later
   res.send(fakeData.history)
 })
+
+app.post('/login', async (req, res) => {
+  let {
+    username,
+    password
+  } = req.body;
+
+  let isAuthenticated = false;
+
+  pool.query(
+    `SELECT * FROM users WHERE username = $1`, [username], (err, results) => {
+      if (err) {
+        throw err
+      }
+      if (results.rows.length > 0) {
+        console.log(results.rows[0])
+        let hashed = results.rows[0].password;
+        function compareHash(password, hashed) {
+          return bcrypt.compareSync(password, hashed)
+        }
+        if (compareHash(password, hashed)) {
+          isAuthenticated = true;
+          res.status(200).send('something')
+        } else {
+          res.status(400)
+        }
+      }
+    }
+  )
+})
+
+function checkAuthenticated(req, res, next){
+  if (req.isAuthenticated()){
+    return res.redirect('/profile');
+  }
+  next();
+}
+
+function checkNotAuthenticated(req, res, next){
+  if (req.isAuthenticated()){
+    return next();
+  }
+  res.redirect('/login');
+}
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
