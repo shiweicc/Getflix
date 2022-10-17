c533f0127cdbdf07937a79873c23af12c11cbfc3
Shiwei Chen <shiweichenus@gmail.com>
2022-10-17 08:55:50 -0500

    Add fontawesome package for button icon

--- client/src/components/profile/history/subComponents/history_carousel.jsx
+++ client/src/components/profile/history/subComponents/history_carousel.jsx
@@ -34,7 +34,7 @@ const Carousel = (props) => {
                 <div className="main-card-title">
                   {movie.original_title}
                 </div>
-                <button className="watched_btn">Watched</button>
+                <button className="watched_btn">X</button>
               </div>
               </SwiperSlide>
             )
