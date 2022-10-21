97c5ab4a2481d3cd12b04f53e7a1e8c7e0faeda8
Shiwei Chen <shiweichenus@gmail.com>
2022-10-20 22:10:43 -0500

    Change watch_btn_click prop's name

--- client/src/components/main/main.js
+++ client/src/components/main/main.js
@@ -103,7 +103,7 @@ function Main(props) {
         />
       <Movies
         movieList = {groupedMovies}
-        updateWatchedList={props.updateWatchedList}
+        updateHistory={props.updateHistory}
       />
     </div>
   );
