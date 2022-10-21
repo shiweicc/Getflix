9f43e7c5d818bc74c11e63ec310075426c046da7
Shiwei Chen <shiweichenus@gmail.com>
2022-10-20 19:31:42 -0500

    Completed dynamically display watched movies in history carousel and added navigation for buttons

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -39,4 +39,35 @@ const Profile = (props) => {
   );
 }
 
+// class Profile extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {history: this.props.history};
+//     // this.getHistory = this.getHistory.bind(this)
+//   }
+
+//   componentDidMount() {
+//     // this.getHistory(1);
+//     console.log('props in Profile: ', this.props.history);
+//   }
+
+//   render() {
+//     const historyList = this.state.history.length;
+//     let history;
+//     if (historyList) {
+//       history = <History watchedList={this.state.history}/>
+//     } else {
+//       history = <h2 className='no-history-title'>No Watch History</h2>
+//     }
+
+//     return (
+//       <div className="profile">
+//        <img className='profile-logo' alt='logo' src={pic} ></img>
+//       <button className='profile-backBtn' onClick={() => {this.props.logout()}}> Home </button>
+//       {history}
+//       </div>
+//     );
+//   }
+// }
+
 export default Profile;
