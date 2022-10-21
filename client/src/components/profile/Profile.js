2fb7ec2184b72a9eb4b16136e869b6d2677200b8
Shiwei Chen <shiweichenus@gmail.com>
2022-10-21 12:26:02 -0500

    Completed remove btn function and add Clear History btn CSS

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,17 +1,32 @@
 import React from "react";
-import { useNavigate } from 'react-router-dom';
 import History from "./history/History.js";
 import pic from '../../getflixLogo.png';
+import { useLocation, useNavigate } from 'react-router-dom';
 import './profile.css';
 
 const Profile = (props) => {
   const navigate = useNavigate();
+
   const navigateToUpdateUsername = () => {
     navigate('/updateUserName');
   }
+
   const navigateToUpdatePwd = () => {
     navigate('/updateUserPwd');
   }
+
+  const navigateMainPage = () => {
+    navigate('/main');
+  }
+
+  const historyList = props.history.length;
+  let history;
+  if (historyList) {
+    history = <History history={props.history} removeEachMovie={props.removeEachMovie} removeAllMovies={props.removeAllMovies}/>
+  } else {
+    history = <h2 className='no-history-title'>No Watch History</h2>
+  }
+
   return (
     <div className="profile">
      <img className='profile-logo' alt='logo' src={pic} ></img>
