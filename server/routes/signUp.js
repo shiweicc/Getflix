6e5927b1f0b342f910577d576e69188e6f98c423
adilovm <miras9191@gmail.com>
2022-10-21 17:40:26 -0700

    auth tracker with localStorage, logou and back navigation

--- server/routes/signUp.js
+++
@@ -1,5 +0,0 @@
-// const express = require('express')
-// const router = express.Router()
-// const { signup } = require('../controllers/Auth')
-// router.route('/signup').post(signup)
-// module.exports = router
