347d760fa235993a85c518d54c527c05e244565d
Gene_Yang <genehcyang@gmail.com>
2022-10-25 22:12:26 -0700

    Quick fix for main service get movies

--- server/server.js
+++ server/server.js
@@ -31,9 +31,14 @@ app.post('/signup', async(req, res)=>{
 })
 
 app.get('/main', (req,res) => {
-  //set up to go to microservice later
-  console.log('test')
-  res.send(fakeData.movies)
+  let url = `http://54.183.28.106:3002/main`
+  axios.get(url)
+    .then((response) => {
+      res.send(response.data.data)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
 })
 
 
