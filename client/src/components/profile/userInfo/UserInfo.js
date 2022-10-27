d3a163f7ba87aeb6042a96a97f4ce753458f29f2
Shiwei Chen <shiweichenus@gmail.com>
2022-10-27 10:44:11 -0500

    Add CSS style for user info

--- client/src/components/profile/userInfo/UserInfo.js
+++ client/src/components/profile/userInfo/UserInfo.js
@@ -4,9 +4,11 @@ import React from "react";
 const UserInfo = (props) => {
   return (
     <div className='userinfo'>
-      <h2 className='userinfo-title'>User Information</h2>
-      <p className='userinfo-username'>User name: <em>{props.userName}</em></p>
-      <p className='userinfo-email'>User email: <em>xxx</em></p>
+      <h2 className='userinfo-title'>Account Information</h2>
+      <p className='userinfo-username-title'>Username:</p>
+      <p className='userinfo-username'>{props.userName}</p>
+      <p className='userinfo-email-title'>Email:</p>
+      <p className='userinfo-email'>xxx@gmail.com</p>
 
     </div>
   );
